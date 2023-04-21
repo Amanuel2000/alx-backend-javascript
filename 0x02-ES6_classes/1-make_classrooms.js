18ba154ad4298fd4e1052a8a9df4e5cffca59423
Amanuel2000 <amanuelf44@gmail.com>
2023-04-21 20:06:06 +0300

    fix eslint

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initialzeRooms() {
-    let classA = new ClassRoom(19);
-    let classB = new ClassRoom(20);
-    let classC = new ClassRoom(34);
-    const resultList = [classA, classB, classC];
-    return (resultList);
+  const classA = new ClassRoom(19);
+  const classB = new ClassRoom(20);
+  const classC = new ClassRoom(34);
+  const resultList = [classA, classB, classC];
+  return (resultList);
 }
--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,25 +1,26 @@
 export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    get name(){
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(nm){
-        this._name = nm;
-    }
+  set name(nm) {
+    this._name = nm;
+  }
 
-    get code(){
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(cd){
-        this._code = cd;
-    }
-    displayFullCurrency(){
-        return `${this.name}(${this.code})`;
-    }
+  set code(cd) {
+    this._code = cd;
+  }
+
+  displayFullCurrency() {
+    return `${this.name}(${this.code})`;
+  }
 }
